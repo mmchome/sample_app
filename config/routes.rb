1badf5b7b00e4d46ad659413a1d4300cdb84c728
Miriam Mchome <mirimchome@gmail.com>
2013-12-01 13:33:17 +0300

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root  'static_pages#home' 
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
@@ -9,7 +10,12 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-
+  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   
   # The priority is based upon order of creation: first created -> highest priority.
